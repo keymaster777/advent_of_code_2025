aef6123b5b534404dc5495ca2c67dc9a4742b652
Riley Cleary <riley.cleary@datasite.com>
2025-12-01 13:20:47 -0700

    Day 1 solutions

---
+++ day_1/part_1.rb
@@ -0,0 +1,17 @@
+INPUT_PATH="part_1_input.txt"
+DUMMY_INPUT_PATH="part_1_dummy.txt"
+
+lines = IO.read(INPUT_PATH).split("\n")
+
+dial_val = 50
+result = 0
+lines.each do |instruction|
+  chars = instruction.chars
+  direction = chars.shift
+  val = chars.join.to_i
+  val *= -1 unless direction == "R"
+  dial_val = (dial_val + val) % 100
+  result += 1 if dial_val == 0
+end
+
+puts result
---
+++ day_1/part_2.rb
@@ -0,0 +1,24 @@
+INPUT_PATH="part_1_input.txt"
+DUMMY_INPUT_PATH="part_1_dummy.txt"
+lines = IO.read(INPUT_PATH).split("\n")
+
+dial_val = 50
+result = 0
+
+lines.each do |line|
+  instruction = line.chars
+  direction = instruction.shift
+  val = instruction.join.to_i
+
+  wrap_count = (val / 100.0).floor
+  remainder = val % 100
+  remainder *= -1 unless direction == "R"
+  no_limit_dial = dial_val + remainder
+
+  wrap_count +=1 if (no_limit_dial > 99 || (no_limit_dial <= 0 && dial_val != 0))
+
+  dial_val = no_limit_dial % 100
+  result += wrap_count
+end
+
+puts result
